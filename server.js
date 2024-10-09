338cba2c793dcbbb2430960349125aae048b342f
Vrushabhpatel17 <165209362+Vrushabhpatel17@users.noreply.github.com>
2024-10-09 14:30:38 -0400

    initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 WEB322 – Assignment 02
 I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
-Name: Vrushabhkumar Patel
+Name: vrushabhkumar patel
 Student ID: 122714223
 Date: 2024-09-10
 
